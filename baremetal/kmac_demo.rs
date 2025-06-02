fc4040eb226e4d9fd87e8a7ccc8d4693ae0afa3d
Chris Frantz <cfrantz@google.com>
2025-06-02 07:30:50 -0700

    Add demo using kmac driver
    Signed-off-by: Chris Frantz <cfrantz@google.com>

--- baremetal/kmac_demo.rs
+++ baremetal/kmac_demo.rs
@@ -1,16 +1,18 @@
 use zerocopy::IntoBytes;
 use base::{println, HexEncode};
 use drivers::kmac::Kmac;
+use traits::digest::{
+    DigestInit, DigestOp,
+    Sha3_512,
+};
 
-use kmac::enums::{EntropyMode, Kstrength, Mode};
 
 pub fn demo() {
     let mut kmac = Kmac::new();
 
-    kmac.configure(EntropyMode::SwMode, Kstrength::L512, Mode::Sha3).expect("configure");
-    kmac.start();
-    kmac.absorb(b"test");
-    let result = kmac.squeeze::<16>().expect("squeeze");
+    let mut hasher = kmac.init(Sha3_512).expect("init");
+    hasher.update(b"test").expect("update");
+    let result = hasher.finalize().expect("finalize");
 
     println!("The SHA3-512 digest of 'test' is {}", HexEncode(result.as_bytes()));
 }
